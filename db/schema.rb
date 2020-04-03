c5c0753a1b5236fa15f982129ea052175e8d7ead
Taylor Swinton <taylorswinton1@gmail.com>
2020-04-03 18:38:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200403183602) do
+ActiveRecord::Schema.define(version: 20200403183506) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20200403183602) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
-    t.         "inventory"
   end
 
 end
